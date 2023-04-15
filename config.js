7affdf2ec91e12ba3bd8948ceabc8ebe131f06f6
Daniel Victoriano <djose1164@gmail.com>
2023-04-14 22:01:16 -0400

    Init

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "djose1164",
+        password: "Kirito08.",
+        database: "flashcard"
+    },
+    listPerPage: 10
+}
+
+module.exports = config;
